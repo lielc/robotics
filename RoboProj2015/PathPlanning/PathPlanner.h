e4bfeeabc814b518a8921b87f7b19a1aab89ad4c
shanianvaim <shaniavnaim@gmail.com>
2015-06-19 10:36:48 -0700

    commit

--- RoboProj2015/PathPlanning/PathPlanner.h
+++ RoboProj2015/PathPlanning/PathPlanner.h
@@ -1,17 +1,30 @@
 /*
  * PathPlanner.h
  *
- *  Created on: Jun 17, 2015
+ *  Created on: Jun 16, 2015
  *      Author: colman
  */
 
 #ifndef PATHPLANNER_H_
 #define PATHPLANNER_H_
 
+#include "node.h"
+#include <iostream>
+#include <iomanip>
+#include <queue>
+#include <string>
+#include <math.h>
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std;
+
 class PathPlanner {
 public:
 	PathPlanner();
 	virtual ~PathPlanner();
+	string pathFind(const int xStart, const int yStart, const int xEnd,
+const int yEnd);
 };
 
 #endif /* PATHPLANNER_H_ */
