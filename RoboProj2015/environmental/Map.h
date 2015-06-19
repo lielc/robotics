8c72a269aef74899b6f2f64c7e958b6219c4fb59
denisya <denis.yanovsky2207@gmail.com>
2015-06-19 04:06:38 -0700

    Tried to read config file

--- RoboProj2015/environmental/Map.h
+++ RoboProj2015/environmental/Map.h
@@ -7,11 +7,20 @@
 
 #ifndef MAP_H_
 #define MAP_H_
+#include <string>
+using namespace std;
 
 class Map {
+	int _mapResolutionCM;
+	int _gridResolutionCM;
+	//char _mapGrid[][];
+
 public:
 	Map();
 	virtual ~Map();
+	void loadMapToGrid(string mapPath);
+	void convertWorldLocationToGridCell();
+	void convertGridCellToWorldLocation();
 };
 
 #endif /* MAP_H_ */
