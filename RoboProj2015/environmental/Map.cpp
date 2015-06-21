832bf9a811b46026f3dff43cd8d236a04fe8313f
denisya <denis.yanovsky2207@gmail.com>
2015-06-21 12:49:33 -0700

    Added some small detail to the map

--- RoboProj2015/environmental/Map.cpp
+++ RoboProj2015/environmental/Map.cpp
@@ -12,9 +12,19 @@ Map::Map() {
 
 }
 
-void loadMapToGrid(string mapPath)
+void blowUpMap()
 {
 
+}
+
+void Map::loadMap(string mapPath)
+{
+	//decode
+	  unsigned error = lodepng::decode(_image, _width, _height, mapPath);
+
+	  //if there's an error, display it
+	  /*if(error)
+		  cout << "decoder error " << error << ": " << lodepng_error_text(error);*/
 }
 
 Map::~Map() {
--- RoboProj2015/environmental/Map.h
+++ RoboProj2015/environmental/Map.h
@@ -8,17 +8,21 @@
 #ifndef MAP_H_
 #define MAP_H_
 #include <string>
+#include "../Utils/lodepng.h"
+#include <stdlib.h>
+#include <stdio.h>
 using namespace std;
 
-class Map {
-	int _mapResolutionCM;
-	int _gridResolutionCM;
-	//char _mapGrid[][];
+const int FREE = 0;
+const int OCCUPIED = 1;
 
+class Map {
+	vector<unsigned char> _image;
+	unsigned _width, _height;
 public:
 	Map();
 	virtual ~Map();
-	void loadMapToGrid(string mapPath);
+	void loadMap(string mapPath);
 	void convertWorldLocationToGridCell();
 	void convertGridCellToWorldLocation();
 };
