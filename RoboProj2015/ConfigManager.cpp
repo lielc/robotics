1a26c9d634ebe734a7372ede92c181fd987ff740
denisya <denis.yanovsky2207@gmail.com>
2015-07-08 11:39:39 -0700

    Added the Map implementation and updated the configuration manager to have return functions for the data we read from the file

--- RoboProj2015/ConfigManager.cpp
+++ RoboProj2015/ConfigManager.cpp
@@ -11,6 +11,51 @@ ConfigManager::ConfigManager() {
 	initializeConfParameters();
 }
 
+int ConfigManager::getGoalLocationX()
+{
+	return _goal.X;
+}
+
+int ConfigManager::getGoalLocationY()
+{
+	return _goal.Y;
+}
+
+int ConfigManager::getStartLocationX()
+{
+	return _startLocation.X;
+}
+
+int ConfigManager::getStartLocationY()
+{
+	return _startLocation.Y;
+}
+
+int ConfigManager::getRobotDepth()
+{
+	return _robotSize.depth;
+}
+
+int ConfigManager::getRobotWidth()
+{
+	return _robotSize.width;
+}
+
+int ConfigManager::getGridResolution()
+{
+	return _gridResolutionCM;
+}
+
+double ConfigManager::getMapResolution()
+{
+	return _mapResolutionCM;
+}
+
+string ConfigManager::getMapPath()
+{
+	return _mapPath;
+}
+
 string getSectionNameFromCurrLine(string currLine)
 {
 	size_t pos = currLine.find(" ");
@@ -93,7 +138,7 @@ void ConfigManager::initializeConfParameters()
 		}
 		else if (sectionName == MAP_RESOLUTION_SETION)
 		{
-			this->_mapResolutionCM = atoi(currLine.substr(sectionName.length() + 1).c_str());
+			this->_mapResolutionCM = atof(currLine.substr(sectionName.length() + 1).c_str());
 		}
 		else
 		{
