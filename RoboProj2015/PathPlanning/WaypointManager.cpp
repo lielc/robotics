0450bcf6e3abd97a0c4251cf786419b72c1289cb
shanianvaim <shaniavnaim@gmail.com>
2015-07-11 11:01:33 -0700

    way Points

--- RoboProj2015/PathPlanning/WaypointManager.cpp
+++ RoboProj2015/PathPlanning/WaypointManager.cpp
@@ -6,41 +6,13 @@
  */
 
 #include "WaypointManager.h"
-#include "PathPlanner.h"
-#include <iostream>
-#include <iomanip>
-#include <queue>
-#include <string>
-#include <math.h>
-#include <ctime>
-#include <stdlib.h>
-
-using namespace std;
 
 WaypointManager::WaypointManager() {
+	// TODO Auto-generated constructor stub
 
 }
 
 WaypointManager::~WaypointManager() {
+	// TODO Auto-generated destructor stub
 }
 
-string getPath(int xStart, int yStart, int xEnd, int yEnd)
-{
-	return ((new PathPlanner)->pathFind(xStart, yStart, xEnd, yEnd));
-}
-void selectedWayPoints()
-{
-	int xStart=0; //need to get from ConfigManager
-	int yStart=0; //need to get from ConfigManager
-	int xEnd=100; //need to get from ConfigManager
-	int yEnd=100; //need to get from ConfigManager
-
-	string path = getPath(xStart,yStart,xEnd, yEnd);
-	for(int i=0; i<(path.length()-1);i++)
-	{
-		if(!(path[i]==path[i+1]))
-		{
-			//TO DO need to add add particle check
-		}
-	}
-}
--- RoboProj2015/PathPlanning/WaypointManager.h
+++ RoboProj2015/PathPlanning/WaypointManager.h
@@ -8,22 +8,10 @@
 #ifndef WAYPOINTMANAGER_H_
 #define WAYPOINTMANAGER_H_
 
-#include <iostream>
-#include <iomanip>
-#include <queue>
-#include <string>
-#include <math.h>
-#include <ctime>
-#include <stdlib.h>
-
-using namespace std;
-
 class WaypointManager {
 public:
 	WaypointManager();
 	virtual ~WaypointManager();
-	string getPath(int xStart, int yStart, int xEnd, int yEnd);
-	void selectedWayPoints();
 };
 
 #endif /* WAYPOINTMANAGER_H_ */
